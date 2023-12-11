a21de234bb7bebc33f3d8d438f814943cb700bc8
unknown <ouattara.abraham2019@gmail.com>
2023-12-11 16:36:09 +0100

    change the gap

--- src/components/ReseauSociaux.js
+++ src/components/ReseauSociaux.js
@@ -18,7 +18,7 @@ function ReseauSociaux() {
     <div className='text-[1.1vw] w-full flex justify-center '>
         {
                 dataReseaux.map(({link,id,icon})=>
-                <a className="p-2 mr-5 rounded-full border-[2px] group hover:bg-slate-100" key={id} href={link} target='blanc'>
+                <a className="p-2 mr-5 rounded-full border-2 group hover:bg-slate-100" key={id} href={link} target='blanc'>
                   {icon}
                 </a>
                 )
